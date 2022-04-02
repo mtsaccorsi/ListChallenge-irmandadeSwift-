eb5dbba75b23cb68499764940d81b60eee570e21
Matheus Accorsi <mts.accorsi@gmail.com>
2022-04-02 15:00:51 -0300

    Initial Commit

---
+++ ListChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListChallenge
+//
+//  Created by Matheus Accorsi on 02/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListChallenge/ListChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListChallengeApp.swift
+//  ListChallenge
+//
+//  Created by Matheus Accorsi on 02/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ListChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
